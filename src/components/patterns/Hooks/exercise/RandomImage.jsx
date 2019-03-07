24205e8b5cf529309cc7195479403252e8034daa
andie5 <andieasa@googlemail.com>
2019-03-07 21:37:21 +0000

    Hooks exercise

--- src/components/patterns/Hooks/exercise/RandomImage.jsx
+++ src/components/patterns/Hooks/exercise/RandomImage.jsx
@@ -1,28 +1,47 @@
 import React, { useState, useEffect } from "react";
 
-class RandomImage extends React.Component {
-  state = {
-    imageUrl: undefined
-  };
+const RandomImage = () => {
+  const [imageUrl, setImageUrl] = useState();
 
-  componentDidMount = () => {
-    this.fetchRandomImage();
-  };
-
-  fetchRandomImage = async () => {
+  const fetchRandomImage = async () => {
     const response = await fetch(
       "https://jsonplaceholder.typicode.com/photos?_limit=100"
     );
     const data = await response.json();
     const image = data[Math.floor(Math.random() * data.length)];
-    this.setState({ imageUrl: image.thumbnailUrl });
+    setImageUrl(image.thumbnailUrl);
   };
 
-  render() {
-    const { imageUrl } = this.state;
+  useEffect(() => {
+    fetchRandomImage();
+  }, []);
 
-    return <img src={imageUrl} />;
-  }
-}
+  return <img src={imageUrl} />;
+};
+
+// class RandomImage extends React.Component {
+//   state = {
+//     imageUrl: undefined
+//   };
+
+//   componentDidMount = () => {
+//     this.fetchRandomImage();
+//   };
+
+//   fetchRandomImage = async () => {
+//     const response = await fetch(
+//       "https://jsonplaceholder.typicode.com/photos?_limit=100"
+//     );
+//     const data = await response.json();
+//     const image = data[Math.floor(Math.random() * data.length)];
+//     this.setState({ imageUrl: image.thumbnailUrl });
+//   };
+
+//   render() {
+//     const { imageUrl } = this.state;
+
+//     return <img src={imageUrl} />;
+//   }
+// }
 
 export default RandomImage;
